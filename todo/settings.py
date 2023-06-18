02dd02e06b63d5b31703dd0dd32d0a5975f70c0e
Antonio Oliveira - DEV <63135415+3636-sergioharpazo@users.noreply.github.com>
2023-06-17 23:34:17 -0300

    Update settings.py

--- todo/settings.py
+++ todo/settings.py
@@ -139,7 +139,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-#STATIC_URL = '/static/'
+#STATIC_URL = 'tasks/static/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
